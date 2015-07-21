f2a39d61309c4aa6bb3096f1ba591f4731e6570d
ZhluktenkoDmytro <d1mnewz@gmail.com>
2015-07-21 16:21:18 +0300

    added critical score handler

--- BinaryTennis.cs
+++ BinaryTennis.cs
@@ -18,6 +18,7 @@ namespace ConsoleApplication10
             
             obj.PrintResultOfGame();
 
+            
             Console.ReadLine();
         }
     }
@@ -29,11 +30,16 @@ namespace ConsoleApplication10
         public BinaryTennis()
         {
             rnd = new Random();
+            this.CriticalScore = false;
+            this.PlayerOneScore = 0;
+            this.PlayerZeroScore = 0;
         }
         Random rnd;
         public List<Serve> results = new List<Serve>();
         public int PlayerOneScore;
         public int PlayerZeroScore;
+        public bool CriticalScore; // to handle 20-20 / 10-10 / etc situation
+
         public void AddServe(Serve obj)
         {
             if (obj.Winner.Equals(false))
@@ -46,16 +52,71 @@ namespace ConsoleApplication10
             }
             results.Add(obj);
         }
+
+        enum CriticalScores
+        {
+            Lose = -2, Less, Equal, More, Win
+        }
+        public void CriticalScoreHandle()
+        {
+            CriticalScores PlayerZeroResult = 0;
+            
+            Serve tmpServe;
+            for (int i = 0; ; i++)
+            {
+                tmpServe = new Serve().GetRandomServe(rnd);
+                AddServe(tmpServe);
+                switch (tmpServe.Winner)
+                {
+                   case false:
+                        PlayerZeroResult += 1;
+                        if (PlayerZeroResult == CriticalScores.Win)
+                            return;
+                        break;
+                    case true:
+                        PlayerZeroResult -= 1;
+                        if (PlayerZeroResult == CriticalScores.Lose)
+                            return;
+                        break;
+                }
+
+                
+            }
+        }
         public void PlayRandomGame()
         {
             for (int i = 0; PlayerOneScore != 21 && PlayerZeroScore != 21; i++)
             {
+                if (this.CriticalScore == true)
+                {
+                    CriticalScoreHandle();
+                    break;
+                }
                 AddServe(new Serve().GetRandomServe(rnd));
+                if (this.PlayerOneScore.Equals(20) && this.PlayerZeroScore.Equals(20))
+                {
+                    this.CriticalScore = true;
+                }
             }
-           
 
+            // test part for criticalScore ; it generates 20: 20 score
+            // 
+            //for (int i = 0; PlayerOneScore != 21 && PlayerZeroScore != 21; i++)
+            //{
+            //    if (this.CriticalScore == true)
+            //    {
+            //        CriticalScoreHandle();
+            //        break;
+            //    }
+            //    AddServe(new Serve() { Winner = Convert.ToBoolean((i & 1) == 0), ServeLog = new List<bool>() });
+            //    if (this.PlayerOneScore.Equals(20) && this.PlayerZeroScore.Equals(20))
+            //    {
+            //        this.CriticalScore = true;
+            //    }
 
+            //}
         }
+
         public void PrintResultOfGame()
         {
             Console.WriteLine(String.Format("Player 0 : {0} \t Player 1 : {1}", PlayerZeroScore, PlayerOneScore));
@@ -66,42 +127,27 @@ namespace ConsoleApplication10
     public class Serve
     {
         public List<bool> ServeLog = new List<bool>();
-        public bool Winner { private set; get; }
+        public bool Winner {set; get; }
         
-        public Serve GetRandomServe(Random rnd)
+        public Serve GetRandomServe(Random rnd) // test tool
         {
             List<bool> tmp = new List<bool>();
-            tmp.Add(rnd.NextDouble() >= 0.5);
-            tmp.Add(rnd.NextDouble() >= 0.5);
-            tmp.Add(rnd.NextDouble() >= 0.5);
-            tmp.Add(rnd.NextDouble() >= 0.5);
-            tmp.Add(rnd.NextDouble() >= 0.5);
-            tmp.Add(rnd.NextDouble() >= 0.5);
-            tmp.Add(rnd.NextDouble() >= 0.5);
-            // Range 1: [0.0 ... 0.5]
-            //Range 2: [0.5 ... 1.0]
-            //|Range 1| = |Range 2|
-            return new Serve() { ServeLog = tmp, Winner = GetWinnerOfServe(tmp) };
+
+            for (int i = 0; i < rnd.Next(2, 15); i++)
+            {
+                tmp.Add(rnd.NextDouble() >= 0.5);
+            }
+                // Range 1: [0.0 ... 0.5]
+                //Range 2: [0.5 ... 1.0]
+                //|Range 1| = |Range 2|
+                return new Serve() { ServeLog = tmp, Winner = GetWinnerOfServe(tmp) };
         }
 
         private static bool GetWinnerOfServe(List<bool> servelog) // test tool
         {
-            return servelog[servelog.Count - 1 - 1];
+            return servelog[servelog.Count - 1 - 1]; // servelog.Count - 1 - 1 == winner 
         }
-        private void SetWinnerOfServe()
-        { 
-            int @false = 0;
-            foreach (var el in ServeLog)
-            {
-                if (el.Equals(false))
-                    @false++;
-            }
-            if (@false > (int)ServeLog.Count / 2) // to add 20:21 situation handle
-            {
-                Winner = false;
-            }
-            else Winner = true;
-        }
+        
         public void PrintResults() // test tool
         {
             int zero = 0;
